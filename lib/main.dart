4a93683840dc138c7f1a345ab1f46873483f9383
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2025-04-22 20:25:34 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:contador_provider/providers/contador_provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ContadorProvider(),
+      child: const MyApp()
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Contador Global',
+      theme: ThemeData(        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: const ContadorPagina(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ContadorPagina extends StatefulWidget {
+  const ContadorPagina({super.key});
+
+  @override
+  State<ContadorPagina> createState() => _ContadorPaginaState();
+}
+class _ContadorPaginaState extends State<ContadorPagina> {
+
+  @override
+  Widget build(BuildContext context) {
+    final contador = Provider.of<ContadorProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Contador Global'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Contador atual:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '${contador.contador}',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: contador.incrementar, child: const Icon(Icons.add)
+                ),
+                ElevatedButton(
+                  onPressed: contador.decrementar, child: const Icon(Icons.remove)
+                )
+              ],
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
